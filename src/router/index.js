165058815851ddc0367eec47039e33c8b9bc1433
h1ldebrand <semenchuk@flabers.com.ua>
2018-11-07 16:37:43 +0200

    video 5 ready

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,47 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 
+import Login from '@/components/Auth/Login'
+import Registration from '@/components/Auth/Registration'
+import Orders from '@/components/User/Orders'
+import NewAd from '@/components/Ads/NewAd'
+import Home from '@/components/Home'
+import AdList from '@/components/Ads/AdList'
+// import Ad from '@/components/Ads/Ad'
+
 Vue.use(Router)
 
 export default new Router({
-  routes: []
+    routes: [
+        {
+            path: '/home',
+            name: 'home',
+            component: Home
+        },
+        {
+            path: '/login',
+            name: 'login',
+            component: Login
+        },
+        {
+            path: '/registration',
+            name: 'registration',
+            component: Registration
+        },
+        {
+            path: '/orders',
+            name: 'orders',
+            component: Orders
+        },
+        {
+            path: '/new',
+            name: 'newAd',
+            component: NewAd
+        },
+        {
+            path: '/list',
+            name: 'list',
+            component: AdList
+        }
+    ]
 })
